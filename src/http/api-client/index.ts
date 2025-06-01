6fd7e366e63e14f0ad3b5d2704e12cc4ef9e5520
99mini <itman390@gmail.com>
2025-06-01 11:59:03 +0900

    release: v0.0.1-alpha.12

--- src/http/api-client/index.ts
+++ src/http/api-client/index.ts
@@ -1,5 +1,4 @@
 import ApiClient from './api-client';
 import type {ApiClientOptions} from './api-client';
 
-export default ApiClient;
-export {ApiClientOptions};
+export {ApiClient, ApiClientOptions};

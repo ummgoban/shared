0f419459f70c17cafd462d4dfc29d833eb5cc6b7
99mini <itman390@gmail.com>
2025-06-05 22:40:52 +0900

    release: 0.0.4-canary.1

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ export * from './lib';
 // react
 export * from './react';
 
-// react-native export from /dist/react-native/index.js
-// export * from './react-native';
+// react-native
+export * from './react-native';

0f5bd48a3d5262e3272a20fa3bdf618b80a2263f
99mini <43674669+99mini@users.noreply.github.com>
2025-08-08 11:36:05 +0900

    feat(lib/constants): weekday enum (#30)

--- src/lib/constants/weekday.const.ts
+++ src/lib/constants/weekday.const.ts
@@ -11,3 +11,27 @@ export const dayMap: Record<Weekday, string> = {
   SATURDAY: '토요일',
   SUNDAY: '일요일',
 };
+
+/**
+ * @description
+ * - 상수로 정의된 요일을 enum으로 정의
+ * - 날짜간 대소 비교에 사용
+ * @example
+ * ```ts
+ * const mondayString: Weekday = "MONDAY"
+ * const fridayString: Weekday = "FRIDAY"
+ * const mondayEnum: WeekdayEnum = WeekdayEnum[mondayString];
+ * const fridayEnum: WeekdayEnum = WeekdayEnum[fridayString];
+ * console.log(mondayEnum > fridayEnum); // false
+ * ```
+ *
+ */
+export enum WeekdayEnum {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
